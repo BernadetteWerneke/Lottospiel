a40e900918c724272c10908b8e035970f6102442
Bernadette <120725476+BernadetteWerneke@users.noreply.github.com>
2023-04-28 15:02:14 +0200

    friday project mandatory done

--- Praxisaufgabe_Swift_1.5/FruitShop.swift
+++ Praxisaufgabe_Swift_1.5/FruitShop.swift
@@ -10,4 +10,29 @@ import Foundation
 func fruitShop() {
     // Schreibe deinen Code für die erste Aufgabe hier
     
+    var fruits: Dictionary = ["Apfel": 1.99, "Kiwi": 0.90, "Banane": 0.50, "Mango": 2.49, "Papaya": 2.20]
+    
+    //Präsentation Angebot
+    print("Unsere heutigen Angebote:")
+    for (fruits, preis) in fruits{
+        print("\(fruits) : \(preis) Euro")
+    }
+    
+    //Kundenangaben
+    print("Guten Tag!")
+    print("Welche Frucht möchten Sie haben?")
+    if let inputFruits: String = readLine() {
+        
+        print("Wieviel Stück möchten Sie haben?")
+        if let inputAnzahl = Double (readLine()!){
+            
+            if fruits[inputFruits] != nil {
+                
+                //Endbetrag berechnen
+                let fru = fruits[inputFruits]
+                let totalBetrag = inputAnzahl * Double(fru!)
+                print("Ihr Einkauf beträgt \(totalBetrag) Euro.")
+            }
+        }
+    }
 }
--- Praxisaufgabe_Swift_1.5/LottoZahlen.swift
+++ Praxisaufgabe_Swift_1.5/LottoZahlen.swift
@@ -9,4 +9,27 @@ import Foundation
 
 func lottoZahlenGenerieren()  {
     // Schreibe deinen Code für die erste Aufgabe hier
+    
+    // random 6 Lottozahlen auswählen
+    var lottozahlen: [Int] = []
+    var einmal: Bool = true
+    
+    for i in 1...6 {
+        var randomZahl = (Int.random(in: 1..<50))
+        lottozahlen.append(randomZahl)
+    
+        //check, ob lotto doppelt vorkommt
+            if lottozahlen.contains(randomZahl) {
+                einmal = false
+                
+                //falls looto einmalig, dann anhängen
+                if einmal {
+                    lottozahlen.append(randomZahl)
+                }
+        }
+    }
+    lottozahlen.sort()
+    print(lottozahlen)
+    
+    
 }
